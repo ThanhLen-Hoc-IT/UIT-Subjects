e5ef53296ff1951e0d1152ddc9d957e95849b7e5
KLEN\thanh <24520949@gm.uit.edu.vn>
2025-09-25 15:37:57 +0700

    Code

---
+++ Foundational-Subjects/NT106/TH/LAB/Lab01/Lab01-Bai01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab01
+{
+    public partial class Lab01_Bai01 : Form
+    {
+        public Lab01_Bai01()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_sum_Click(object sender, EventArgs e)
+        {
+           int num1=0, num2= 0, result = 0;
+            bool isNum1Valid = int.TryParse(tb_num1.Text, out num1);
+            bool isNum2Valid = int.TryParse(tb_num2.Text, out num2);
+            if (!isNum1Valid || !isNum2Valid)
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên hợp lệ!");
+                return;
+            }
+            result = num1 + num2;
+            tb_result.Text = result.ToString();
+
+
+        }
+    }
+}
---
+++ Foundational-Subjects/NT106/TH/LAB/Lab01/MENU.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class MENU : Form
+    {
+        public MENU()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_Bai1_Click(object sender, EventArgs e)
+        {
+            Lab01_Bai01 f = new Lab01_Bai01();   // tạo form Bài 1
+            f.ShowDialog();        // hiển thị form Bài 1
+        }
+    }
+}
